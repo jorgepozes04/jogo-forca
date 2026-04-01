e4c9377a42370122632dadbaf613da0ae0339458
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 15:40:03 -0300

    implementa instancia em memoria de RepositoryFactory

--- src/br/edu/iff/jogoforca/embdr/BDRRepositoryFactory.java
+++ src/br/edu/iff/jogoforca/embdr/BDRRepositoryFactory.java
@@ -1,6 +1,30 @@
 package br.edu.iff.jogoforca.embdr;
 
+import br.edu.iff.bancodepalavras.dominio.palavra.PalavraRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
 import br.edu.iff.jogoforca.RepositoryFactory;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.jogoforca.dominio.rodada.RodadaRepository;
 
 public class BDRRepositoryFactory implements RepositoryFactory {
+
+    @Override
+    public PalavraRepository getPalavraRepository() {
+        throw new UnsupportedOperationException("Unimplemented method 'getPalavraRepository'");
+    }
+
+    @Override
+    public TemaRepository getTemaRepository() {
+        throw new UnsupportedOperationException("Unimplemented method 'getTemaRepository'");
+    }
+
+    @Override
+    public RodadaRepository getRodadaRepository() {
+        throw new UnsupportedOperationException("Unimplemented method 'getRodadaRepository'");
+    }
+
+    @Override
+    public JogadorRepository getJogadorRepository() {
+        throw new UnsupportedOperationException("Unimplemented method 'getJogadorRepository'");
+    }
 }
--- src/br/edu/iff/jogoforca/emmemoria/MemoriaRepositoryFactory.java
+++ src/br/edu/iff/jogoforca/emmemoria/MemoriaRepositoryFactory.java
@@ -1,6 +1,45 @@
 package br.edu.iff.jogoforca.emmemoria;
 
+import br.edu.iff.bancodepalavras.dominio.palavra.PalavraRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
 import br.edu.iff.jogoforca.RepositoryFactory;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.jogoforca.dominio.rodada.RodadaRepository;
 
 public class MemoriaRepositoryFactory implements RepositoryFactory {
+
+    private static MemoriaRepositoryFactory soleInstance = null;
+
+    private PalavraRepository palavraRepository;
+    private TemaRepository temaRepository;
+    private RodadaRepository rodadaRepository;
+    private JogadorRepository jogadorRepository;
+
+    public MemoriaRepositoryFactory getSoleInstance() {
+        if (MemoriaRepositoryFactory.soleInstance == null) {
+            MemoriaRepositoryFactory.soleInstance = new MemoriaRepositoryFactory();
+        }
+
+        return MemoriaRepositoryFactory.soleInstance;
+    }
+
+    private MemoriaRepositoryFactory() {
+    }
+
+    public PalavraRepository getPalavraRepository() {
+        return palavraRepository;
+    }
+
+    public TemaRepository getTemaRepository() {
+        return temaRepository;
+    }
+
+    public RodadaRepository getRodadaRepository() {
+        return rodadaRepository;
+    }
+
+    public JogadorRepository getJogadorRepository() {
+        return jogadorRepository;
+    }
+
 }
