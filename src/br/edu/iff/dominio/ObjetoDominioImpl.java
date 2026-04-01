5bb00fa7aa01af5b4f49aacab388ee166982fcc7
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 10:18:56 -0300

    implementa Item

--- src/br/edu/iff/dominio/ObjetoDominioImpl.java
+++ src/br/edu/iff/dominio/ObjetoDominioImpl.java
@@ -1,9 +1,9 @@
 package br.edu.iff.dominio;
 
 public abstract class ObjetoDominioImpl implements ObjetoDominio {
-    private final Long id;
+    private Long id;
 
-    public ObjetoDominioImpl(Long id){
+    public ObjetoDominioImpl(Long id) {
         this.id = id;
     }
 
--- src/br/edu/iff/jogoforca/dominio/rodada/Item.java
+++ src/br/edu/iff/jogoforca/dominio/rodada/Item.java
@@ -1,6 +1,164 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
+import br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import br.edu.iff.bancodepalavras.dominio.palavra.Palavra;
 import br.edu.iff.dominio.ObjetoDominioImpl;
 
 public class Item extends ObjetoDominioImpl {
+
+    private Palavra palavra;
+    private boolean[] posicoesDescobertas;
+    private String palavraArriscada = null;
+
+    public static Item criar(long id, Palavra palavra) {
+        return new Item(id, palavra);
+    }
+
+    public static Item reconstruir(long id, Palavra palavra, int[] posicoesDescobertas, String palavraArriscada) {
+        return new Item(id, palavra, posicoesDescobertas, palavraArriscada);
+    }
+
+    private Item(long id, Palavra palavra) {
+        super(id);
+
+        setPalavra(palavra);
+        setPosicoesDescobertas(palavra.getTamanho());
+    }
+
+    private Item(long id, Palavra palavra, int[] posicoesDescobertas, String palavraArriscada) {
+        super(id);
+
+        setPalavra(palavra);
+        setPosicoesDescobertas(palavra.getTamanho());
+
+        for (int i = 0; i < posicoesDescobertas.length; i++) {
+            if (posicoesDescobertas[i] == 1) {
+                this.posicoesDescobertas[i] = true;
+            }
+        }
+
+        this.palavraArriscada = palavraArriscada;
+
+    }
+
+    public Palavra getPalavra() {
+        return this.palavra;
+    }
+
+    public Letra[] getLetrasDescobertas() {
+        Letra[] letrasDescobertas = new Letra[qtdeLetrasDescobertas()];
+        int idxPosicoes = 0;
+
+        for (int i = 0; i < this.posicoesDescobertas.length; i++) {
+            if (this.posicoesDescobertas[i]) {
+                letrasDescobertas[idxPosicoes] = this.palavra.getLetra(i);
+                idxPosicoes++;
+            }
+        }
+
+        return letrasDescobertas;
+    }
+
+    public Letra[] getLetrasEncobertas() {
+        Letra[] letrasEncobertas = new Letra[qtdeLetrasEncobertas()];
+        int idxPosicoes = 0;
+
+        for (int i = 0; i < this.posicoesDescobertas.length; i++) {
+            if (!this.posicoesDescobertas[i]) {
+                letrasEncobertas[idxPosicoes] = this.palavra.getLetra(i);
+                idxPosicoes++;
+            }
+        }
+
+        return letrasEncobertas;
+    }
+
+    public int qtdeLetrasEncobertas() {
+        int qtdeEncobertas = 0;
+
+        for (boolean pos : this.posicoesDescobertas) {
+            if (!pos)
+                qtdeEncobertas++;
+        }
+
+        return qtdeEncobertas;
+    }
+
+    public int qtdeLetrasDescobertas() {
+        int qtdeDescobertas = 0;
+
+        for (boolean pos : this.posicoesDescobertas) {
+            if (pos)
+                qtdeDescobertas++;
+        }
+
+        return qtdeDescobertas;
+    }
+
+    public int calcularPontosLetrasEncobertas(int valorPorLetraEncoberta) {
+        if (valorPorLetraEncoberta < 0)
+            throw new IllegalArgumentException("O valor por letra encoberta não pode ser negativo!");
+
+        return qtdeLetrasEncobertas() * valorPorLetraEncoberta;
+    }
+
+    public boolean descobriu() {
+        return (acertou() || qtdeLetrasEncobertas() == 0);
+    }
+
+    public void exibir(Object contexto) {
+    }
+
+    protected boolean tentar(char codigo) {
+        int[] posicoes = this.palavra.tentar(codigo);
+
+        for (int i = 0; i < posicoes.length; i++) {
+            this.posicoesDescobertas[posicoes[i]] = true;
+        }
+
+        return posicoes.length > 0;
+    }
+
+    protected void arriscar(String palavra) {
+        if (palavraArriscada != null)
+            throw new IllegalStateException("Você pode arriscar apenas uma vez.");
+
+        setPalavraArriscada(palavra);
+    }
+
+    public String getPalavraArriscada() {
+        return palavraArriscada;
+    }
+
+    public boolean arriscou() {
+        return this.palavraArriscada != null;
+    }
+
+    public boolean acertou() {
+        if (!arriscou())
+            return false;
+
+        return this.palavra.comparar(this.palavraArriscada);
+    }
+
+    private void setPalavra(Palavra palavra) {
+        if (palavra == null)
+            throw new IllegalArgumentException("A palavra nao pode ser nula");
+
+        this.palavra = palavra;
+    }
+
+    private void setPosicoesDescobertas(int tamanho) {
+        if (tamanho < 0)
+            throw new IllegalArgumentException("O tamanho nao pode ser negativo");
+
+        this.posicoesDescobertas = new boolean[tamanho];
+    }
+
+    private void setPalavraArriscada(String palavraArriscada) {
+        if (palavraArriscada == null || palavraArriscada.isEmpty())
+            throw new IllegalArgumentException("A palavra nao pode ser nula nem vazia");
+
+        this.palavraArriscada = palavraArriscada;
+    }
 }
