75efc6e3bd408179f5861ee28936200937d8cf8b
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 06:36:01 -0300

    implementa repositório em memória - Jogador

--- src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
@@ -1,6 +1,38 @@
 package br.edu.iff.jogoforca.dominio.jogador.embdr;
 
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
 import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.repository.RepositoryException;
 
 public class BDRJogadorRepository implements JogadorRepository {
+
+    @Override
+    public long getProximoId() {
+        throw new UnsupportedOperationException("Unimplemented method 'getProximoId'");
+    }
+
+    @Override
+    public Jogador getPorId(long id) {
+        throw new UnsupportedOperationException("Unimplemented method 'getPorId'");
+    }
+
+    @Override
+    public Jogador getPorNome(String nome) {
+        throw new UnsupportedOperationException("Unimplemented method 'getPorNome'");
+    }
+
+    @Override
+    public void inserir(Jogador jogador) throws RepositoryException {
+        throw new UnsupportedOperationException("Unimplemented method 'inserir'");
+    }
+
+    @Override
+    public void atualizar(Jogador jogador) throws RepositoryException {
+        throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
+    }
+
+    @Override
+    public void remover(Jogador jogador) throws RepositoryException {
+        throw new UnsupportedOperationException("Unimplemented method 'remover'");
+    }
 }
--- src/br/edu/iff/jogoforca/dominio/jogador/emmemoria/MemoriaJogadorRepository.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/emmemoria/MemoriaJogadorRepository.java
@@ -1,6 +1,62 @@
 package br.edu.iff.jogoforca.dominio.jogador.emmemoria;
 
+import java.util.ArrayList;
+
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
 import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.repository.RepositoryException;
 
 public class MemoriaJogadorRepository implements JogadorRepository {
+
+    private static MemoriaJogadorRepository soleInstance = null;
+    private ArrayList<Jogador> pool = new ArrayList<>();
+
+    public MemoriaJogadorRepository getSoleInstance() {
+        if (MemoriaJogadorRepository.soleInstance == null) {
+            MemoriaJogadorRepository.soleInstance = new MemoriaJogadorRepository();
+        }
+        return MemoriaJogadorRepository.soleInstance;
+    }
+
+    private MemoriaJogadorRepository() {
+    }
+
+    @Override
+    public long getProximoId() {
+        return this.pool.size();
+    }
+
+    @Override
+    public Jogador getPorId(long id) {
+        return this.pool.get((int) id);
+    }
+
+    @Override
+    public Jogador getPorNome(String nome) {
+        for (Jogador jogador : pool) {
+            if (jogador.getNome().equals(nome)) {
+                return jogador;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void inserir(Jogador jogador) throws RepositoryException {
+        this.pool.add(jogador);
+    }
+
+    @Override
+    public void atualizar(Jogador jogador) throws RepositoryException {
+        for (int i = 0; i < this.pool.size(); i++) {
+            if (jogador.getId() == this.pool.get(i).getId()) {
+                this.pool.set(i, jogador);
+            }
+        }
+    }
+
+    @Override
+    public void remover(Jogador jogador) throws RepositoryException {
+        this.pool.remove(jogador);
+    }
 }
