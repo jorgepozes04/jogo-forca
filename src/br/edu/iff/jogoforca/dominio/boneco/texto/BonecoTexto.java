b2ce9d267e86a64290bcd05f214fa79168b303cd
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 09:52:28 -0300

    implementa boneco texto

--- src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
@@ -1,4 +1,22 @@
 package br.edu.iff.jogoforca.dominio.boneco.texto;
 
-public class BonecoTexto {
+import br.edu.iff.jogoforca.dominio.boneco.Boneco;
+
+public class BonecoTexto implements Boneco {
+    private static BonecoTexto soleInstance = null;
+
+    private BonecoTexto() {
+    }
+
+    public static BonecoTexto getSoleInstance() {
+        if (BonecoTexto.soleInstance == null) {
+            BonecoTexto.soleInstance = new BonecoTexto();
+        }
+        return BonecoTexto.soleInstance;
+    }
+
+    @Override
+    public void exibir(Object contexto, int partes) {
+        System.out.println("Boneco com " + partes + " partes.");
+    }
 }
--- src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTextoFactory.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTextoFactory.java
@@ -1,6 +1,22 @@
 package br.edu.iff.jogoforca.dominio.boneco.texto;
 
+import br.edu.iff.jogoforca.dominio.boneco.Boneco;
 import br.edu.iff.jogoforca.dominio.boneco.BonecoFactory;
 
-public interface BonecoTextoFactory extends BonecoFactory {
+public class BonecoTextoFactory implements BonecoFactory {
+    private static BonecoTextoFactory soleInstance = null;
+
+    private BonecoTextoFactory() {
+    }
+
+    public static BonecoTextoFactory getSoleInscance() {
+        if (BonecoTextoFactory.soleInstance == null) {
+            BonecoTextoFactory.soleInstance = new BonecoTextoFactory();
+        }
+        return BonecoTextoFactory.soleInstance;
+    }
+
+    public Boneco getBoneco() {
+        return BonecoTexto.getSoleInstance();
+    }
 }
