68c570e209ec2b02be4f465de86a2b206f9f0d37
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 09:54:14 -0300

    implementa boneco imagem

--- src/br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagem.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagem.java
@@ -1,4 +1,22 @@
 package br.edu.iff.jogoforca.dominio.boneco.imagem;
 
-public class BonecoImagem {
+import br.edu.iff.jogoforca.dominio.boneco.Boneco;
+
+public class BonecoImagem implements Boneco {
+    private static BonecoImagem soleInstance = null;
+
+    private BonecoImagem() {
+    }
+
+    public static BonecoImagem getSoleInstance() {
+        if (BonecoImagem.soleInstance == null) {
+            BonecoImagem.soleInstance = new BonecoImagem();
+        }
+        return BonecoImagem.soleInstance;
+    }
+
+    @Override
+    public void exibir(Object contexto, int partes) {
+        System.out.println("Boneco com " + partes + " partes.");
+    }
 }
--- src/br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagemFactory.java
+++ src/br/edu/iff/jogoforca/dominio/boneco/imagem/BonecoImagemFactory.java
@@ -1,6 +1,22 @@
 package br.edu.iff.jogoforca.dominio.boneco.imagem;
 
+import br.edu.iff.jogoforca.dominio.boneco.Boneco;
 import br.edu.iff.jogoforca.dominio.boneco.BonecoFactory;
 
-public interface BonecoImagemFactory extends BonecoFactory {
+public class BonecoImagemFactory implements BonecoFactory {
+    private static BonecoImagemFactory soleInstance = null;
+
+    private BonecoImagemFactory() {
+    }
+
+    public static BonecoImagemFactory getSoleInscance() {
+        if (BonecoImagemFactory.soleInstance == null) {
+            BonecoImagemFactory.soleInstance = new BonecoImagemFactory();
+        }
+        return BonecoImagemFactory.soleInstance;
+    }
+
+    public Boneco getBoneco() {
+        return BonecoImagem.getSoleInstance();
+    }
 }
