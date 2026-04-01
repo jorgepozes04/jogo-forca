7c892c9b215ef26db3935b0485683b614dbef5af
Jorge Pozes <jorgepozes027@gmail.com>
2026-04-01 11:00:59 -0300

    implementa relacionados a Rodada

--- src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
+++ src/br/edu/iff/jogoforca/dominio/rodada/RodadaAppService.java
@@ -1,4 +1,66 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
+import java.util.Objects;
+
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorNaoEncontradoException;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.repository.RepositoryException;
+
 public class RodadaAppService {
+
+    private RodadaFactory rodadaFactory;
+    private RodadaRepository rodadaRepository;
+    private JogadorRepository jogadorRepository;
+    private static RodadaAppService soleInstance = null;
+
+    public static void createSoleInstance(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository,
+            JogadorRepository jogadorRepository) {
+        if (RodadaAppService.soleInstance == null)
+            RodadaAppService.soleInstance = new RodadaAppService(rodadaFactory, rodadaRepository, jogadorRepository);
+    }
+
+    public static RodadaAppService getSoleInstance() {
+        return RodadaAppService.soleInstance;
+    }
+
+    private RodadaAppService(RodadaFactory rodadaFactory, RodadaRepository rodadaRepository,
+            JogadorRepository jogadorRepository) {
+        Objects.requireNonNull(rodadaFactory);
+        Objects.requireNonNull(rodadaRepository);
+        Objects.requireNonNull(jogadorRepository);
+
+        this.rodadaFactory = rodadaFactory;
+        this.rodadaRepository = rodadaRepository;
+        this.jogadorRepository = jogadorRepository;
+    }
+
+    public Rodada novaRodada(long idJogador) {
+        Jogador jogador = jogadorRepository.getPorId(idJogador);
+
+        Rodada rodada = rodadaFactory.getRodada(jogador);
+
+        return rodada;
+    }
+
+    public Rodada novaRodada(String nomeJogador) throws JogadorNaoEncontradoException {
+        Jogador jogador = jogadorRepository.getPorNome(nomeJogador);
+
+        if (jogador == null)
+            throw new JogadorNaoEncontradoException(nomeJogador);
+
+        Rodada rodada = rodadaFactory.getRodada(jogador);
+
+        return rodada;
+    }
+
+    public boolean salvarRodada(Rodada rodada) {
+        try {
+            rodadaRepository.inserir(rodada);
+            return true;
+        } catch (RepositoryException e) {
+            return false;
+        }
+    }
+
 }
--- src/br/edu/iff/jogoforca/dominio/rodada/RodadaFactoryImpl.java
+++ src/br/edu/iff/jogoforca/dominio/rodada/RodadaFactoryImpl.java
@@ -1,4 +1,50 @@
 package br.edu.iff.jogoforca.dominio.rodada;
 
-public abstract class RodadaFactoryImpl implements RodadaFactory {
+import br.edu.iff.bancodepalavras.dominio.palavra.PalavraRepository;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.factory.EntityFactory;
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
+
+public abstract class RodadaFactoryImpl extends EntityFactory implements RodadaFactory {
+
+    private TemaRepository temaRepository;
+    private PalavraRepository palavraRepository;
+
+    protected RodadaFactoryImpl(RodadaRepository rodadaRepository, TemaRepository temaRepository,
+            PalavraRepository palavraRepository) {
+        super(rodadaRepository);
+
+        setTemaRepository(temaRepository);
+        setPalavraRepository(palavraRepository);
+    }
+
+    @Override
+    public abstract Rodada getRodada(Jogador jogador);
+
+    protected RodadaRepository getRodadaRepository() {
+        return (RodadaRepository) getRepository();
+    }
+
+    protected TemaRepository getTemaRepository() {
+        return this.temaRepository;
+    }
+
+    protected PalavraRepository getPalavraRepository() {
+        return this.palavraRepository;
+    }
+
+    private void setTemaRepository(TemaRepository temaRepository) {
+        if (temaRepository == null)
+            throw new IllegalArgumentException("O repositorio de tema nao pode ser nulo");
+
+        this.temaRepository = temaRepository;
+    }
+
+    private void setPalavraRepository(PalavraRepository palavraRepository) {
+        if (palavraRepository == null)
+            throw new IllegalArgumentException("O repositorio de palavra nao pode ser nulo");
+
+        this.palavraRepository = palavraRepository;
+    }
+
 }
